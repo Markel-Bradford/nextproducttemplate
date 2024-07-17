d4267dc76ab0f32167aed299b6f74152ae887bca
Markel-Bradford <markel.bradford94@gmail.com>
2024-07-17 17:35:23 -0500

    Adjusted config

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,4 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'postcss-loader',
-        ],
-      });
-    }
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
