e1e919495b4eb6a23b061933ff6b80f8147ed315
Markel-Bradford <markel.bradford94@gmail.com>
2024-07-18 00:21:28 -0500

    Adjusted hero styles for better responsiveness

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -8,22 +8,22 @@ const Hero: React.FC = () => {
 
   return (
     <div className="max-w-[1800px] flex justify-center items-center bg-white mx-auto px-24 max-lg:px-0 mt-12 h-1/3 max-lg:flex-col max-md:w-full">
-      <div className="leading-8 w-full text-black animate-fade">
-        <h1 className="text-4xl font-bold md:text-7xl max-lg:text-center">
+      <div className="leading-8 w-full text-black animate-fade max-lg:my-6">
+        <h1 className="text-[10vw] mb-[5%] font-bold lg:mb-[5%] lg:text-[4.5vw] max-lg:text-center">
           Platinum Threads
         </h1>
-        <p className="text-2xl md:text-5xl max-lg:text-center">
+        <p className="text-[6vw] lg:text-[2.5vw] max-lg:text-center">
           Let your style shine
         </p>
       </div>
-      <div className="flex justify-end w-[50%] lg:w-full max-lg:mt-12">
+      <div className="flex justify-end w-[100%] max-lg:px-9 lg:w-full max-lg:mt-12 ">
         <Image
           width={500}
           height={750}
           priority
           src={imageUrl}
           alt="Model with shades"
-          className="w-[75%] flex animate-slideandfade max-lg:w-full"
+          className="w-[75%] flex animate-slideandfade max-lg:w-[80%] max-lg:mx-auto"
         />
       </div>
     </div>
