9d161ba359db91e7b74bd84e1f15ab3ef3bd7298
Markel-Bradford <markel.bradford94@gmail.com>
2024-07-17 20:21:13 -0500

    Updated file structure and adjust tailwind config

---
+++ src/app/mens/mens.tsx
@@ -0,0 +1,10 @@
+import MensCards from "@/Components/MensCards"
+
+export default function Mens() {
+    return (
+      <div className="min-h-[100vh]">
+      <h1 className="text-center text-4xl mt-12 -mb-12 font-bold">Men&apos;s Apperal</h1>
+          <MensCards />
+        </div>
+      )
+}
