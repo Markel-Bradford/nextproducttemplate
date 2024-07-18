44e662e1fd374eae41c35d5058db1c9b35e48a56
Markel-Bradford <markel.bradford94@gmail.com>
2024-07-17 20:50:03 -0500

    Updated global styles and _app

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,10 +1,11 @@
-import { AppProps } from "next/app";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import "../styles/globals.css";
-import ScrollToTop from "@/ScrollToTop";
+// src/app/_app.tsx
+import { AppProps } from 'next/app';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // Import FontAwesome styles
+import '../styles/globals.css'; // Import global CSS
+import ScrollToTop from '@/ScrollToTop'; // Import ScrollToTop component
 
-config.autoAddCss = false;
+config.autoAddCss = false; // Disable auto-adding FontAwesome CSS
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
