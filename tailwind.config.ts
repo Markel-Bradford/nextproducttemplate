f84c0c4c28d251a96c2ccb9cedb9fe0ee218a1a0
Markel-Bradford <markel.bradford94@gmail.com>
2024-07-17 22:27:58 -0500

    Adjusted path to match Components folder

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/Components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/styles/**/*.css',
   ],
   theme: {
