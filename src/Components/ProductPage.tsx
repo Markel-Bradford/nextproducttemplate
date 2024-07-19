d94d63d1b7fa763669355343ef95106daecb03e4
Markel-Bradford <markel.bradford94@gmail.com>
2024-07-19 13:18:27 -0500

    Adjusted product preview positioning on mobile

--- src/Components/ProductPage.tsx
+++ src/Components/ProductPage.tsx
@@ -35,8 +35,8 @@ const ProductPage: React.FC<ProductPageProps> = (props) => {
   if (!props.product) return null;
 
   return (
-    <div className="fixed max-w-[1800px] mx-auto inset-0 bg-white z-50 flex flex-col items-center justify-center p-5">
-      <button className="absolute bg-black top-28 right-5 text-white text-lg py-3 px-6 border-black border-solid border-2 hover:bg-gray-700 duration-100" onClick={props.onClose}>
+    <div className="max-lg:top-[80px] fixed max-w-[1800px] mx-auto inset-0 bg-white z-50 flex flex-col items-center justify-center p-5">
+      <button className="absolute bg-black top-12 right-5 text-white text-lg py-3 px-6 border-black border-solid border-2 hover:bg-gray-700 duration-100" onClick={props.onClose}>
         Back
       </button>
       {imageLoaded ? (
